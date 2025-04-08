41c535b5b7bde553c88fa19132d8792615d7b895
root <root@debian1.homelab>
2025-04-08 10:35:48 +0000

    literowka changes

--- submit_form.php
+++ submit_form.php
@@ -21,8 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     file_put_contents($filename, json_encode($data, JSON_PRETTY_PRINT));
 
     echo "<script>
-      alert('Oczekujemy 6 z niecierpliwością.\\n\\nPS. Ten forumlarz tak naprawdę nie działa.');
-//      alert(get_current_user());
+      alert('Oczekujemy 6 z niecierpliwością.\\n\\nPS. Ten formularz tak naprawdę nie działa.');
       window.location.href = 'contact.html';
     </script>";
 } else {
